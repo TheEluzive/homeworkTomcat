e73ed2b351d2808a98fee35b7d75f123662e968f
Evgeny Tkach <1eluzive1@gmail.com>
2021-09-27 00:32:06 +0300

    anonymous filter

---
+++ src/main/java/org/example/framework/filter/AnonymousAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package org.example.framework.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.framework.attribute.ContextAttributes;
+import org.example.framework.attribute.RequestAttributes;
+import org.example.framework.security.*;
+
+import java.io.IOException;
+
+public class AnonymousAuthenticationFilter extends HttpFilter {
+    private AuthenticationProvider provider;
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        super.init(config);
+        provider = ((AuthenticationProvider) getServletContext().getAttribute(ContextAttributes.AUTH_PROVIDER_ATTR));
+    }
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        if (!authenticationIsRequired(req)) {
+            super.doFilter(req, res, chain);
+            return;
+        }
+
+        try {
+            final var authentication = provider.authenticate(new AnonymousAuthentication(null));
+            req.setAttribute(RequestAttributes.AUTH_ATTR, authentication);
+        } catch (AuthenticationException e) {
+            res.sendError(401);
+            return;
+        }
+
+        super.doFilter(req, res, chain);
+    }
+
+    private boolean authenticationIsRequired(HttpServletRequest req) {
+        final var existingAuth = (AnonymousAuthentication) req.getAttribute(RequestAttributes.AUTH_ATTR);
+
+        if (existingAuth == null || !existingAuth.isAuthenticated()) {
+            return true;
+        }
+
+        return AnonymousAuthentication.class.isAssignableFrom(existingAuth.getClass());
+    }
+}

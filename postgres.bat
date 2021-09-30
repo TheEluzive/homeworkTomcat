6ee3a9af096daa89d0692a1f25c28f6b1e4e05bf
Evgeny Tkach <1eluzive1@gmail.com>
2021-09-30 21:32:02 +0300

    closed #1

--- postgres.bat
+++
@@ -1,3 +0,0 @@
-docker container run -p 5555:5432 -e POSTGRES_USER=app -e POSTGRES_PASSWORD=1234 -e POSTGRES_DB=app -v "E:\HW\homeworkTomcat\docker-entrypoint-initdb.d":/docker-entrypoint-initdb.d postgres 
-
-pause
